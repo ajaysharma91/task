7c5917f787f72c1cb7316e77f44ce598d69a821c
ajaysharma91 <ajparashar111@gmail.com>
2020-07-29 16:46:37 +0530

    Thursday commit

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 
     var dataSet = [
-        [ "Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ],
-        [ "Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750" ],
-        [ "Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000" ],
-        [ "Cedric Kelly", "Senior Javascript Developer", "Edinburgh", "6224", "2012/03/29", "$433,060" ],
-        [ "Airi Satou", "Accountant", "Tokyo", "5407", "2008/11/28", "$162,700" ],
-        [ "Brielle Williamson", "Integration Specialist", "New York", "4804", "2012/12/02", "$372,000" ],
-        [ "Herrod Chandler", "Sales Assistant", "San Francisco", "9608", "2012/08/06", "$137,500" ],
-        [ "Rhona Davidson", "Integration Specialist", "Tokyo", "6200", "2010/10/14", "$327,900" ],
-        [ "Colleen Hurst", "Javascript Developer", "San Francisco", "2360", "2009/09/15", "$205,500" ],
-        [ "Sonya Frost", "Software Engineer", "Edinburgh", "1667", "2008/12/13", "$103,600" ],
-        [ "Jena Gaines", "Office Manager", "London", "3814", "2008/12/19", "$90,560" ],
-        [ "Ajay Flynn", "Ajay Lead", "Edinburgh", "9497", "2013/03/03", "$342,000" ],
-        [ "Ajay Flynn", "Ajay Lead", "Edinburgh", "9497", "2013/03/03", "$342,000" ],
-
-        [ "Ajay Flynn", "Ajay Lead", "Edinburgh", "9497", "2013/03/03", "$342,000" ],
-
-        [ "Ajay Flynn", "Ajay Lead", "Edinburgh", "9497", "2013/03/03", "$342,000" ],
+        [ "Tiger Nixon", "System Architect", "Edinburgh", "2011/04/25" ],
+        [ "Tiger Nixon", "System Architect", "Edinburgh", "2011/04/25" ],
+        [ "Tiger Nixon", "System Architect", "Edinburgh", "2011/04/25" ],
+        [ "Tiger Nixon", "System Architect", "Edinburgh", "2011/04/25" ],
 
     ];
-     var arr = [ "Quinn Flynn", "Support Lead", "Edinburgh", "9497", "2013/03/03", "$342,000" ];
-     dataSet.push(arr);
+    //  var arr = [ "Quinn Flynn", "Support Lead", "Edinburgh", "9497", "2013/03/03", "$342,000" ];
+    //  dataSet.push(arr);
      console.log("dataSet");
+     let arr=[];
+     let title;
+     let type;
+     let associate;
+     let assignTo;
+     let queue;
+     let date;
+     let time;
+     console.log(arr[0]);
 
+    function submitForm(event) {
+        // e.preventDefault();
+        title = event.target.elements.title.value;
+        type = event.target.elements.type.value;
+        associate = event.target.elements.title.value;
+        assignTo = event.target.elements.assign.value;
+        queue = event.target.elements.queue.value;
+        date = event.target.elements.date.value;
+        time = event.target.elements.time.value;
+        let dueDate = date +" "+time;
+        //dataSet.push(dueDate);
+         return false;
+       }
     $(document).ready(function() {
         $('#example').DataTable( {
             data: dataSet,
@@ -49,13 +58,45 @@
                 });
               },
             columns: [
-                { title: "Name" },
-                { title: "Position" },
-                { title: "Office" },
-                { title: "Extn." },
-                { title: "Start date" },
-                { title: "Salary" }
+                { title: "Title" },
+                { title: "Type" },
+                { title: "Associate with-" },
+                { title: "DueDate" }
+                
             ]
         } );
     } );
 
+    $(document).ready(function(){
+      var date_input=$('input[name="date"]'); //our date input has the name "date"
+      var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+      date_input.datepicker({
+          format: 'mm/dd/yyyy',
+          container: container,
+          todayHighlight: true,
+          autoclose: true,
+      })
+  })
+
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "370px";
+    document.getElementById("main").style.marginRight = "370px";
+    document.getElementById("main1").style.marginRight = "370px";
+  
+    document.body.style.backgroundColor = "white";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+    document.body.style.backgroundColor = "white";
+  }
+  
+    
+  //     $(function() {
+  //   $('#title').on('input', function() {
+  //     this.value = this.value.match(/^[A-Za-z][A-Za-z0-9 -]*$/, '');
+  //   });
+  // });
+  
+  
